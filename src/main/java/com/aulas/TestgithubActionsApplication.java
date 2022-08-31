45050a4cacaebc773fa75017b1ea90077a8c3c16
Vilson <instrutorvilsonmoro@gmail.com>
2022-08-31 10:55:28 -0300

    Primeiro commit

---
+++ src/main/java/com/aulas/TestgithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.aulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestgithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestgithubActionsApplication.class, args);
+	}
+
+}
